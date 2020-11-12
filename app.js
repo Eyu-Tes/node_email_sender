30913c256b1d973a6a6e4b24ed117e994b283254
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2020-11-12 10:56:32 +0300

    added HTTP request logger middleware for development mode

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 // load environment variables from a .env file into process.env
 dotenv.config({path: './config/config.env'})
 
 const app = express()
+
+// HTTP request logger (development mode only)
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 5000
 
